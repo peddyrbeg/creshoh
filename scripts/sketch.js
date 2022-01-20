aa02b38149a823d8f1df98ff7c09f5136d651bf5
peddyrbeg <85888409+peddyrbeg@users.noreply.github.com>
2022-01-20 16:09:02 +0000

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -105,7 +105,7 @@ function draw () {
 	}
 
 	else {
-		image(jeantDyMie, cnv.width * 0.5, cnv.height * 0.3);
+		image(jeantDyMie, cnv.width * 0.5, cnv.height * 0.25);
 		text(score + "/" + items.length * 3, cnv.width * 0.5, cnv.height * 0.475)
 		if (score > 9) text("Jeant dy mie!", cnv.width * 0.5, cnv.height * 0.55);
 		else text("Prow reesht!", cnv.width * 0.5, cnv.height * 0.55);
